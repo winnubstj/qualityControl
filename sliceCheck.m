5ff4ed838416b1c7f8b618576bb98501e66b4e38
winnubstj <winnubstj@janelia.hhmi.org>
2016-04-07 17:11:59 -0400

    created starting sliceCheck that will control for slice thicknes

---
+++ sliceCheck.m
@@ -0,0 +1,10 @@
+function [ output_args ] = sliceCheck( tileInfo, threshold )
+%sliceCheck Takes tile position info from this tile and previous tiles to
+%check that slice thickness is within expected range.
+
+
+
+
+
+end
+
